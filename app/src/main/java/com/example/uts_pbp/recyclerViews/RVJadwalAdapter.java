4f798e092b5d907f3a9da7fce460a888f058912f
System_01 <190710233@students.uajy.ac.id>
2021-10-23 20:40:16 +0700

    v3.0 - (1)Pemasangan alert untuk menampilkan detail jadwal, (2)Bolding tulisan recycleview produk

--- app/src/main/java/com/example/uts_pbp/recyclerViews/RVJadwalAdapter.java
+++ app/src/main/java/com/example/uts_pbp/recyclerViews/RVJadwalAdapter.java
@@ -1,13 +1,22 @@
 package com.example.uts_pbp.recyclerViews;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.text.Html;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.uts_pbp.R;
 import com.example.uts_pbp.databinding.RvItemJadwalBinding;
 import com.example.uts_pbp.entity.Jadwal;
@@ -24,11 +33,41 @@ public class RVJadwalAdapter extends RecyclerView.Adapter<RVJadwalAdapter.ViewHo
         public ViewHolder(@NonNull RvItemJadwalBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+            binding.getRoot().setOnClickListener(this::onClick);
         }
         public void bind(Jadwal item) {
             binding.setJdwl(item);
             binding.executePendingBindings();
         }
+
+        public void onClick (View view){
+            onNoteClick(getAdapterPosition());
+        }
+    }
+
+    //pemanggil tampilan detail jadwal
+    public void onNoteClick (int position){
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.alert_jadwal);
+
+        TextView text1 = dialog.findViewById(R.id.tv_detail_tanggal);
+        text1.setText(listJadwal.get(position).getTanggal());
+        TextView text2 = dialog.findViewById(R.id.tv_detail_pelayanan);
+        text2.setText(listJadwal.get(position).getPelayanan());
+        TextView text3 = dialog.findViewById(R.id.tv_detail_petugas);
+        text3.setText(listJadwal.get(position).getPetugas());
+        ImageView image = dialog.findViewById(R.id.image_detail_petugas);
+        Jadwal.loadImage(image, listJadwal.get(position).getImgUrl());
+
+        TextView text4 = dialog.findViewById(R.id.button_detail_tutup);
+        text4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 
     public RVJadwalAdapter(List<Jadwal> listJadwal, Context context) {
